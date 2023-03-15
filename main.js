2a7561ee71f5d4ff5b1845794e6959f727de75b7
Allyson Thales <allysonthales@protonmail.com>
2023-03-15 11:38:15 -0300

    otimização do css e salvando estado do darkmode em localstorage

--- main.js
+++ main.js
@@ -1,5 +1,22 @@
+window.onload = getThemeFromLocalstorage;
+
 function toogleMode() {
   const html = document.documentElement;
+  let dark = !html.classList.contains("dark");
 
   html.classList.toggle("dark");
+  console.log(dark);
+
+  saveThemeToLocalstorage(dark);
+}
+
+function saveThemeToLocalstorage(theme) {
+  localStorage.setItem("theme", theme);
+}
+
+function getThemeFromLocalstorage() {
+  const theme = localStorage.getItem("theme");
+  if (String(theme) == "true") {
+    document.documentElement.classList.add("dark");
+  }
 }
